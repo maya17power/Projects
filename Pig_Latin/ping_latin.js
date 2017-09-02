5bc1840925335f883cb8292b97a976f70bb22734
maya17power <tavo1.ortega1@gmail.com>
2017-09-02 10:21:42 -0700

    updating ping latin project.

---
+++ Pig_Latin/ping_latin.js
@@ -0,0 +1,43 @@
+/*
+Pig Latin
+Translate the provided string to pig latin.
+
+Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+
+If a word begins with a vowel you just add "way" to the end.
+
+Input strings are guaranteed to be English words in all lowercase.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Array.prototype.indexOf()
+Array.prototype.push()
+Array.prototype.join()
+string.prototype.substring() // is similar to slice(), substring() can go backwards were slice() cannot.
+String.prototype.substr() // substr second perameter is the move up count for example: substr(10,4) 4 is the number of characters it will move up from 10th position starting from 0.
+String.prototype.split()
+Run tests (ctrl + enter)
+
+
+*/
+
+
+function translatePigLatin(str) {
+   var word = str.substr(0,1);
+   var vowel = ["a","e","i","o","u"];
+   if(word == vowel.filter(function(val){return val == word;})){
+     return str + "way";
+   }
+     return "no way";
+}
+
+//translatePigLatin("consonant");
+
+
+//translatePigLatin("california"); //should return "aliforniacay".
+//translatePigLatin("paragraphs"); //should return "aragraphspay".
+//translatePigLatin("glove"); //should return "oveglay".
+//translatePigLatin("algorithm"); //should return "algorithmway".
+translatePigLatin("eight"); //should return "eightway".
