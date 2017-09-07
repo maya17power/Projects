32cdf10a184cb0670965d75237a44f08466895bb
maya17power <tavo1.ortega1@gmail.com>
2017-09-07 12:42:21 -0700

    Cleaning code out to make it more readable.

--- DNA_Pairing/dna_pairing.js
+++ DNA_Pairing/dna_pairing.js
@@ -22,29 +22,42 @@ String.prototype.split()
 */
 
 
+// function pairElement(str) {
+//   var count = 0;
+//   var e = str.split('');
+//   var base = [['A','T'],['C','G'],['T','A'],['G','C']];
+//   var match = [];
+//   //console.log("Current count: "+ count );
+//   //console.log(base[0]);// display per letter
+//   for(var i = 0; i < e.length; i++){
+//     for(var j = 0; j < base.length; j++){
+//         if(e[i] === base[j][0]){
+//           match.push(base[j]);
+//           console.log("Current count: "+ match.length );
+//         }
+//           console.log("no match found");
+//     }//end for loop slave
+//   }//end for loop master
+//   console.log(match);
+//   return match;
+// }
+
 function pairElement(str) {
   var count = 0;
   var e = str.split('');
   var base = [['A','T'],['C','G'],['T','A'],['G','C']];
   var match = [];
-  //console.log("Current count: "+ count );
-  //console.log(base[0]);// display per letter
   for(var i = 0; i < e.length; i++){
-    for(var j = 0; j < base.length; j++){
-        if(e[i] === base[j][0]){
-          match.push(base[j]);
-          console.log("Current count: "+ match.length );
-        }
-          console.log("no match found");
-    }//end for loop slave
-  }//end for loop master
-  console.log(match);
+      for(var j = 0; j < base.length; j++){
+          if(e[i] === base[j][0]){
+            match.push(base[j]);
+          }
+      }
+  }
   return match;
 }
 
-//pairElement("GCG");
-
-
+pairElement("GCG");
 
 //pairElement("ATCGA");// should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
 //pairElement("TTGAG");// should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
