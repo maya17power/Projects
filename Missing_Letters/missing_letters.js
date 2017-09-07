b5237830d9d8be49da7b9b2bcdeafe4967aaf8d5
maya17power <tavo1.ortega1@gmail.com>
2017-09-07 12:48:28 -0700

    Updating missing letters project code.

--- Missing_Letters/missing_letters.js
+++ Missing_Letters/missing_letters.js
@@ -8,10 +8,10 @@ Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write you
 
 Here are some helpful links:
 
-*/
 
 String.prototype.charCodeAt()
 String.fromCharCode()
+*/
 
 function fearNotLetter(str) {
   return str;
