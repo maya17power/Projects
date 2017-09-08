4535a3aaff46d3ae5feeefbcfe3e91b5ed4d388f
maya17power <tavo1.ortega1@gmail.com>
2017-09-08 12:37:25 -0700

    Updated Boo hoo project to repo.

--- Boo_Hoo/boo_hoo.js
+++ Boo_Hoo/boo_hoo.js
@@ -14,7 +14,7 @@ Boolean Objects
 
 function booWho(bool) {
   // What is the new fad diet for ghost developers? The Boolean.
-  return bool;
+  return bool === true || bool === false ? true : false;
 }
 
 booWho(null);
