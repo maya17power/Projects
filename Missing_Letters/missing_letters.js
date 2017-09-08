c891293309a713f3d94f56f774cd053306b09420
maya17power <tavo1.ortega1@gmail.com>
2017-09-07 18:58:48 -0700

    modified code by relocating return undefined at end of function.

--- Missing_Letters/missing_letters.js
+++ Missing_Letters/missing_letters.js
@@ -14,38 +14,50 @@ String.fromCharCode() //String.fromCharCode(65, 66, 67);  // returns "ABC"
 
 */
 
+// function fearNotLetter(str) {
+//   var initChar = str.charCodeAt(0);
+//   var charCount = [];
+//   console.log("string length: " + str.length);//display how many characters we are working with?
+//   console.log("initCharacter: " + initChar); //display the first character on the string?
+//
+//   //add the init character of the string and add the length to return the correct sequence.
+//   for(var i = 0; i < str.length; i++){
+//
+//       if(str.indexOf(String.fromCharCode(initChar + i)) == -1){
+//          return String.fromCharCode(initChar + i);
+//       }
+//          undefined;
+//
+//   }
+//   console.log("character missing: " + charCount);
+//   var myString = String.fromCharCode(str.charCodeAt(0) + 1);
+//
+//   //console.log("convert charCount: " + myString);
+//
+//   // for(var j = 0; j < charCount.lenght; j++){
+//
+//   // }
+//
+//
+//
+//
+//   // else find that character and return it.
+//   // otherwise if all characters found return undefined.
+//
+//   //return str;
+// }
+
 function fearNotLetter(str) {
   var initChar = str.charCodeAt(0);
-  var charCount = [];
-  console.log("string length: " + str.length);//display how many characters we are working with?
-  console.log("initCharacter: " + initChar); //display the first character on the string?
-
   //add the init character of the string and add the length to return the correct sequence.
   for(var i = 0; i < str.length; i++){
-
-      if(str.indexOf(String.fromCharCode(initChar + i)) == -1){
-         return String.fromCharCode(initChar + i);
-      }
-         undefined;
-
+     if(str.indexOf(String.fromCharCode(initChar + i)) == -1){
+        return String.fromCharCode(initChar + i);
+     }
   }
-  console.log("character missing: " + charCount);
-  var myString = String.fromCharCode(str.charCodeAt(0) + 1);
-
-  //console.log("convert charCount: " + myString);
-
-  // for(var j = 0; j < charCount.lenght; j++){
-
-  // }
-
-
-
-
-  // else find that character and return it.
-  // otherwise if all characters found return undefined.
-
-  //return str;
+  return undefined;
 }
+
 
 fearNotLetter("abce");
 
