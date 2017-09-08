d024c1a9e9fe4ed0a1f776299cbb48952125c504
maya17power <tavo1.ortega1@gmail.com>
2017-09-07 18:50:54 -0700

    Completed the missing letters project.

--- Missing_Letters/missing_letters.js
+++ Missing_Letters/missing_letters.js
@@ -15,18 +15,28 @@ String.fromCharCode() //String.fromCharCode(65, 66, 67);  // returns "ABC"
 */
 
 function fearNotLetter(str) {
+  var initChar = str.charCodeAt(0);
   var charCount = [];
   console.log("string length: " + str.length);//display how many characters we are working with?
-  console.log("charCodeAt: " + str.charCodeAt(0)); //display the first character on the string?
-  //charCount.push(str.charCodeAt(0) + 1);
-
-  for(var i = 0; i < str.length; i++){
-      charCount.push(str.charCodeAt(0) + i );
-  }
-
-  console.log("character synch: " charCount);
+  console.log("initCharacter: " + initChar); //display the first character on the string?
 
   //add the init character of the string and add the length to return the correct sequence.
+  for(var i = 0; i < str.length; i++){
+
+      if(str.indexOf(String.fromCharCode(initChar + i)) == -1){
+         return String.fromCharCode(initChar + i);
+      }
+         undefined;
+
+  }
+  console.log("character missing: " + charCount);
+  var myString = String.fromCharCode(str.charCodeAt(0) + 1);
+
+  //console.log("convert charCount: " + myString);
+
+  // for(var j = 0; j < charCount.lenght; j++){
+
+  // }
 
 
 
