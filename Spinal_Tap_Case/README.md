8970d02eb884f05e5f9e81705d802120de841f7c
maya17power <tavo1.ortega1@gmail.com>
2017-09-18 18:26:08 -0700

    Adding Spinal tap case to projects repo.

---
+++ Spinal_Tap_Case/README.md
@@ -0,0 +1,9 @@
+Spinal Tap Case
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+RegExp
+String.prototype.replace()
---
+++ Spinal_Tap_Case/spinal_tap_case.js
@@ -0,0 +1,27 @@
+/*
+Spinal Tap Case
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+RegExp
+String.prototype.replace()
+
+*/
+
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  return str;
+}
+
+spinalCase('This Is Spinal Tap');
+
+
+//spinalCase("This Is Spinal Tap");// should return "this-is-spinal-tap".
+//spinalCase("thisIsSpinalTap");// should return "this-is-spinal-tap".
+//spinalCase("The_Andy_Griffith_Show");// should return "the-andy-griffith-show".
+//spinalCase("Teletubbies say Eh-oh");// should return "teletubbies-say-eh-oh".
+//spinalCase("AllThe-small Things");// should return "all-the-small-things".
