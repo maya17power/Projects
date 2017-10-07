5133effc5ebd569151ba6eaa30bfafbfed3ea167
maya17power <tavo1.ortega1@gmail.com>
2017-10-06 17:08:47 -0700

    Adding Steamroller to projects folder.

---
+++ Steamroller/README.md
@@ -0,0 +1,8 @@
+Steamroller
+Flatten a nested array. You must account for varying levels of nesting.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Array.isArray()
---
+++ Steamroller/steamroller.js
@@ -0,0 +1,25 @@
+/*
+Steamroller
+Flatten a nested array. You must account for varying levels of nesting.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Array.isArray()
+
+*/
+
+function steamrollArray(arr) {
+  // I'm a steamroller, baby
+  
+  return arr;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);
+
+
+//steamrollArray([[["a"]], [["b"]]]);// should return ["a", "b"].
+//steamrollArray([1, [2], [3, [[4]]]]);// should return [1, 2, 3, 4].
+// steamrollArray([1, [], [3, [[4]]]]);// should return [1, 3, 4].
+// steamrollArray([1, {}, [3, [[4]]]]);// should return [1, {}, 3, 4]
