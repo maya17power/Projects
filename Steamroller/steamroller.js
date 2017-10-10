e45c52fd3a082ad2fed2dd0b163ecade344081bf
maya17power <tavo1.ortega1@gmail.com>
2017-10-09 18:46:01 -0700

    Updating steamroller code to projects repo.

--- Steamroller/steamroller.js
+++ Steamroller/steamroller.js
@@ -11,20 +11,11 @@ Array.isArray()
 */
 
 function steamrollArray(arr) {
-  // I'm a steamroller, baby
-  var myArr = [];
-  var i = 0;
-  var current = 0;
-  var myFilter = /[0-9]/g;
-  console.log(arr[3]);
-  console.log(Array.isArray(arr[3]));
-  //myArr.push(arr[0]);
-
-  myArr = arr.map(function(task, index, array){
-    return task[1];
-  });
-
+  var myArr = arr.reduce(function(a, b){
+    return a.concat(Array.isArray(b) ? steamrollArray(b) : b);
+  },[]);
   return myArr;
+  // I'm a steamroller, baby
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
