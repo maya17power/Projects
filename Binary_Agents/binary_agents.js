336c36370df6983eab74d4520f9d8ff9195c0347
maya17power <tavo1.ortega1@gmail.com>
2017-10-10 17:02:48 -0700

    Updating binary agents code to projects.

--- Binary_Agents/binary_agents.js
+++ Binary_Agents/binary_agents.js
@@ -14,7 +14,13 @@ String.fromCharCode()
 */
 
 function binaryAgent(str) {
-  return str;
+  //var decimal = str.charCodeAt();
+  var dArray = str.split(' ');
+  return dArray.map(function(item, index, array){
+    return array[index];
+  });
+
+  //return String.fromCharCode(str.charCodeAt());
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
