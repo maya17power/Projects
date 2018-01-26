1fba006e8689ec6e84fb24bb5025f40bbbe175e2
maya17power <tavo1.ortega1@gmail.com>
2018-01-25 21:50:42 -0800

    finished exact change project.

--- Exact_Change/exact_change.js
+++ Exact_Change/exact_change.js
@@ -16,13 +16,53 @@ Global Object
 
 */
 
+var denom = [
+  { name: 'ONE HUNDRED', val: 100.00},
+  { name: 'TWENTY', val: 20.00},
+  { name: 'TEN', val: 10.00},
+  { name: 'FIVE', val: 5.00},
+  { name: 'ONE', val: 1.00},
+  { name: 'QUARTER', val: 0.25},
+  { name: 'DIME', val: 0.10},
+  { name: 'NICKEL', val: 0.05},
+  { name: 'PENNY', val: 0.01}
+];
+
 function checkCashRegister(price, cash, cid) {
-  var change;
-  // Here is your change, ma'am.
-  if(cash - price !== 0){
-    console.log(cash-price);
+  var change = cash - price;
+
+  var register = cid.reduce(function(acc,curr){
+    acc.total += curr[1];
+    acc[curr[0]] = curr[1];
+    return acc;
+  }, {total: 0});
+
+    if(register.total === change){
+      return "Closed";
+    }
+    if(register.total < change){
+      return "Insufficiant Funds";
+    }
+      var change_arr = denom.reduce(function(acc,curr){
+        var value = 0;
+        while(register[curr.name] > 0 && change >= curr.val){
+          change -= curr.val;
+          register[curr.name] -= curr.val;
+          value += curr.val;
+          change = Math.round(change * 100) / 100;
+        }
+
+        if(value > 0){
+          acc.push([curr.name, value]);
+        }
+
+        return acc;
+      },[]);
+  if(change_arr < 1 || change > 0){
+    return "Insufficient Funds";
   }
-  return change;
+
+  return change_arr;
 }
 
 // Example cash-in-drawer array:
@@ -37,8 +77,6 @@ function checkCashRegister(price, cash, cid) {
 // ["ONE HUNDRED", 100.00]]
 
 checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
-
-
 
 // checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]) //should return an array.return .50 cents
 // checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) //should return a string.Insufficiant funds
